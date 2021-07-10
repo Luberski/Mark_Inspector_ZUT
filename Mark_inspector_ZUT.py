cbadbb4661bdf1c4e9977563aa12fc6f2bc01aa1
Luberski <luberski@int.pl>
2021-07-10 14:30:21 +0200

    Pushing old files

---
+++ Mark_inspector_ZUT.py
@@ -0,0 +1,154 @@
+from bs4 import BeautifulSoup
+from getpass import getpass
+from datetime import datetime
+import requests
+import time
+from pydub import AudioSegment
+from pydub.playback import play
+import os
+
+URL="https://edziekanat.zut.edu.pl/WU/Logowanie2.aspx"
+headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"}
+username = ""
+password = ""
+user = "student"
+ss = None
+data = []
+clear = lambda: os.system('cls')
+
+def check_changes():
+    global data
+
+    old_data = []
+    with open('oceny.txt') as f:
+        for line in f:
+            old_data.append(line.strip().split(';'))
+    f.close
+    
+    for i in range(len(data)):
+        for j in range(2,len(data[0])-1):
+            if(old_data[i][j] != data[i][j]):
+                if(data[i][j] == '2'):
+                    try:
+                        play(AudioSegment.from_mp3("audio/2.mp3"))
+                    except:
+                        print('No audio file. Place your audio files under audio/5.mp3, audio/2.mp3, audio/3+.mp3 in .exe directory')
+                elif(data[i][j] == '5'):
+                    try:
+                        play(AudioSegment.from_mp3("audio/5.mp3"))
+                    except:
+                        print('No audio file. Place your audio files under audio/5.mp3, audio/2.mp3, audio/3+.mp3 in .exe directory')
+                else:
+                    try:
+                        play(AudioSegment.from_mp3("audio/3+.mp3"))
+                    except:
+                        print('No audio file. Place your audio files under audio/5.mp3, audio/2.mp3, audio/3+.mp3 in .exe directory')
+                print(f'{data[i][0]} ocena: {data[i][j]}')
+                input('Press enter to continue...')
+                clear()
+
+    
+
+def saveto_file():
+    with open('oceny.txt', "w", encoding='utf-8') as f:
+        for li in data:
+            for ele in li:
+                f.write(ele + ';')
+            f.write('\n')
+        f.close()
+
+def grab_data():
+    global data
+    
+    session=requests.Session()
+    session.headers.update(headers)
+    req=session.get(URL)
+    soup=BeautifulSoup(req.content, 'html.parser')
+
+    EVENTTARGET=soup.find(id="__EVENTTARGET")['value']
+    EVENTARGUMENT=soup.find(id="__EVENTARGUMENT")['value']
+    VIEWSTATE=soup.find(id="__VIEWSTATE")['value']
+    VIEWSTATEGENERATOR=soup.find(id="__VIEWSTATEGENERATOR")['value']
+
+    payload={
+        "__EVENTTARGET":EVENTTARGET,
+        "__EVENTARGUMENT":EVENTARGUMENT,
+        "__VIEWSTATE":VIEWSTATE,
+        "__VIEWSTATEGENERATOR":VIEWSTATEGENERATOR,
+        "ctl00_ctl00_TopMenuPlaceHolder_TopMenuContentPlaceHolder_MenuTop3_menuTop3_ClientState":"",
+        "ctl00$ctl00$ContentPlaceHolder$MiddleContentPlaceHolder$txtIdent":username,
+        "ctl00$ctl00$ContentPlaceHolder$MiddleContentPlaceHolder$txtHaslo":password,
+        "ctl00$ctl00$ContentPlaceHolder$MiddleContentPlaceHolder$butLoguj":"Zaloguj",
+        "ctl00$ctl00$ContentPlaceHolder$MiddleContentPlaceHolder$rbKto":user
+    }
+
+    req = session.post(URL, data=payload)
+    page = session.get("https://edziekanat.zut.edu.pl/WU/OcenyP.aspx")
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    data_temp = []
+    data = []
+    table = soup.find(id="ctl00_ctl00_ContentPlaceHolder_RightContentPlaceHolder_dgDane")
+
+    try:
+        rows = table.find_all('tr')
+        for row in rows:
+            cols = row.find_all('td')
+            cols = [ele for ele in cols]
+            data_temp.append([ele for ele in cols if ele])
+    except:
+        print('You propably entered wrong credentials')
+        input('restart application')
+        quit()
+
+    data_temp.pop(0)
+
+    for przedm in data_temp:
+        li = list()
+
+        przedm = przedm[:2] + przedm[5:-3]
+
+        for i in range(len(przedm)):
+            li.append(przedm[i].find(text=True).replace(u'\xa0', u'Brak_oceny'))
+
+        data.append(li)
+
+if __name__ == '__main__':
+    username = input('Username: ')
+    password = getpass(prompt='Password: ')
+
+    while(True):
+        try:
+            ss = int(input(f'Refresh rate in s: '))
+            if(ss < 1 and ss > 3600):
+                print("Try value between 1s and 1h")
+                continue
+            break
+        except:
+            print("Not a valid input")
+    
+    grab_data()
+    saveto_file()
+
+    while(True):
+        time.sleep(ss)
+
+        clear()
+        now = datetime.now()
+        print(f'Last refresh: {now.strftime("%H:%M:%S")}')
+
+        grab_data()
+        check_changes()
+        saveto_file()
+
+        
+
+        
+        
+
+
+    
+
+
+
+
